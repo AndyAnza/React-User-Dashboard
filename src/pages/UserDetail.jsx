aca14dbd5e4191b49cf1d984040070ae869ca8ce
Andrea Ramirez <andrea.anza3@gmail.com>
2024-09-29 14:28:49 -0600

    Installed react-router-dom dependency. Configured routing for Login, UsersList and UserDetail pages, deleted unnecessary files.

---
+++ src/pages/UserDetail.jsx
@@ -0,0 +1,7 @@
+export default function UserDetail() {
+  return (
+    <>
+      <h2>User Detail Page</h2>
+    </>
+  );
+}
---
+++ src/pages/UsersList.jsx
@@ -0,0 +1,7 @@
+export default function UsersList() {
+  return (
+    <>
+      <h2>Users List Page</h2>
+    </>
+  );
+}
