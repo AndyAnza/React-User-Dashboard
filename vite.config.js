6314df9d650b94643ad279fe4e986cd5b3821f2d
Andrea Ramirez <andrea.anza3@gmail.com>
2024-10-02 07:30:40 -0600

    feat: DEV and PROD optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      compression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+    ],
+    build: {
+      minify: mode === 'production' ? 'terser' : false,
+      outDir: 'dist',
+      sourcemap: mode === 'production' ? false : true,
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+              return 'style.css';
+            }
+            return assetInfo.name;
+          },
+          entryFileNames: 'bundle.js',
+        },
+      },
+    },
+    server: {
+      hmr: {
+        port: 5273, // Hot Module Replacement port
+      },
+    },
+    publicDir: 'public', // Specify the public directory
+  };
+});
